5716ac731624e0443165ac951b82ba73d5909b3d
Kushagra-002 <kg7741365@gmail.com>
2025-11-17 02:48:55 +0530

    adds 4 more solutions

---
+++ sdelist/sol1.ts
@@ -0,0 +1,3 @@
+// react fibre ek naya tareeke ka react engine hai jo madad krta hai react ko perfomance fix krne me aur faste krne me 
+// using scheduling it priorotizes taks 
+// paises and reintroduces work according to priority
